743552c2e78df7c0eda35bfaf0a7fb618137361d
Yun <liuyun.97@gmail.com>
2017-07-24 19:52:08 +0800

    +++

--- backends/go/src/main/handlers.go
+++ backends/go/src/main/handlers.go
@@ -20,37 +20,39 @@ func (app *App) getNameList(c *gin.Context) {
 func (app *App) createNameList(c *gin.Context) {
 	user := c.MustGet("user").(User)
 	var json Request
-	if c.BindJSON(&json) == nil {
-		nameList := json.Data
-		if nameList.Title == "" || nameList.Names == nil {
-			badRequest(c)
-		}
-		nameList.Creator = user
+	if c.BindJSON(&json) != nil {
+		badRequest(c)
+		return
+	}
 
-		tx := app.DB.Begin()
-		tx.Set("gorm:insert_option", "ON CONFLICT (id) DO NOTHING").Create(&nameList)
-		tx.Exec("INSERT INTO user_name_lists (user_id, name_list_id) VALUES (?, ?)", user.ID, nameList.ID)
-		if tx.Error == nil {
-			tx.Commit()
-			ok(c, nameList)
-		} else {
-			tx.Rollback()
-			badRequest(c)
-		}
+	nameList := json.Data
+	if nameList.Title == "" || nameList.Names == nil {
+		badRequest(c)
+	}
+	nameList.Creator = user
+
+	tx := app.DB.Begin()
+	tx.Set("gorm:insert_option", "ON CONFLICT (id) DO NOTHING").Create(&nameList)
+	tx.Exec("INSERT INTO user_name_lists (user_id, name_list_id) VALUES (?, ?)", user.ID, nameList.ID)
+	if tx.Error == nil {
+		tx.Commit()
+		ok(c, nameList)
 	} else {
+		tx.Rollback()
 		badRequest(c)
 	}
 }
 
 func (app *App) updateNameList(c *gin.Context) {
 	var json Request
-	if c.BindJSON(&json) == nil {
-		nameList := json.Data
-		app.DB.Model(&nameList).Select("Title", "Names", "Visibility").Update(&nameList)
-		ok(c, nameList)
-	} else {
+	if c.BindJSON(&json) != nil {
 		badRequest(c)
+		return
 	}
+
+	nameList := json.Data
+	app.DB.Model(&nameList).Select("Title", "Names", "Visibility").Update(&nameList)
+	ok(c, nameList)
 }
 
 func (app *App) getUser(c *gin.Context) {
@@ -61,38 +63,40 @@ func (app *App) getUser(c *gin.Context) {
 // params: id int, fork bool
 func (app *App) importNameList(c *gin.Context) {
 	user := c.MustGet("user").(User)
-	nid, err1 := strconv.Atoi(c.Param("id"))
-	fork, err2 := strconv.ParseBool(c.Param("fork"))
+	nid, err1 := strconv.Atoi(c.PostForm("id"))
+	fork, err2 := strconv.ParseBool(c.PostForm("fork"))
 	var nameList NameList
-	if app.DB.First(&nameList, nid).Error == nil && err1 != nil && err2 != nil {
-		tx := app.DB.Begin()
-		if !fork {
-			nameList.ID = -1
-			tx.Create(&nameList)
-		}
-		tx.Exec("INSERT INTO user_name_lists (user_id, name_list_id) VALUES (?, ?)", user.ID, nameList.ID)
-		if tx.Error == nil {
-			tx.Commit()
-			ok(c, user)
-		} else {
-			tx.Rollback()
-			badRequest(c)
-		}
+	if app.DB.First(&nameList, nid).Error != nil || err1 != nil || err2 != nil {
+		badRequest(c)
+		return
+	}
+
+	tx := app.DB.Begin()
+	if !fork {
+		nameList.ID = -1
+		tx.Create(&nameList)
+	}
+	tx.Exec("INSERT INTO user_name_lists (user_id, name_list_id) VALUES (?, ?)", user.ID, nameList.ID)
+	if tx.Error == nil {
+		tx.Commit()
+		ok(c, user)
 	} else {
+		tx.Rollback()
 		badRequest(c)
 	}
 }
 
 func (app *App) removeNameList(c *gin.Context) {
 	user := c.MustGet("user").(User)
-	nid, err := strconv.Atoi(c.Param("id"))
+	nid, err := strconv.Atoi(c.PostForm("id"))
 	if err != nil {
-		app.DB.Exec("DELETE FROM user_name_lists WHERE user_id = ? AND name_list_id = ?", user.ID, nid)
-		if app.DB.Error == nil {
-			ok(c, user)
-		} else {
-			badRequest(c)
-		}
+		badRequest(c)
+		return
+	}
+
+	app.DB.Exec("DELETE FROM user_name_lists WHERE user_id = ? AND name_list_id = ?", user.ID, nid)
+	if app.DB.Error == nil {
+		ok(c, user)
 	} else {
 		badRequest(c)
 	}
